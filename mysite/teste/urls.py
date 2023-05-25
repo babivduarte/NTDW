566bcb520e06564798ee9f70f2c95393a3aca1c5
babivduarte <duartebarbara33@gmail.com>
2023-05-24 23:33:08 -0300

    Inserting update option

--- mysite/teste/urls.py
+++ mysite/teste/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path, include        #importando o módulo
-from . import views                 #importa tudo o que tá dentro do arquivo views
+from django.urls import path, include  # importando o módulo
+from . import views  # importa tudo o que tá dentro do arquivo views
 from rest_framework.routers import DefaultRouter
 from .views import *
 from rest_framework.documentation import include_docs_urls
@@ -14,33 +14,35 @@ router.register(r'projetos_enviados', ProjetoEnviadoViewApi)
 router.register(r'projetos_avaliados', ProjetoAvaliadoViewApi)
 
 urlpatterns = [
-    path('',views.index, name='index'),
+    path('', views.index, name='index'),
     path('apis/', include(router.urls)),
     path('docs/', include_docs_urls(title='APIs')),
 
-    path('novoAutor/',views.novoAutor, name='novoAutor'),
-    path('autores/',views.listAutores, name='listAutores'),
+    path('novoAutor/', views.novoAutor, name='novoAutor'),
+    path('autores/', views.listAutores, name='listAutores'),
     path('deletarAutor/<int:id>', views.deleteAutor, name='deletarAutor'),
+    path('alterarAutor/<int:id>', views.alterarAutor, name='alterarAutor'),
 
-    path('novoCronograma/',views.novoCronograma, name='novoCronograma'),
-    path('cronogramas/',views.listCronogramas, name='listCronogramas'),
+    path('novoCronograma/', views.novoCronograma, name='novoCronograma'),
+    path('cronogramas/', views.listCronogramas, name='listCronogramas'),
     path('deletarCronograma/<int:id>', views.deleteCronograma, name='deletarCronograma'),
+    path('alterarCronograma/<int:id>', views.alterarCronograma, name='alterarCronograma'),
 
-    path('novoAvaliador/',views.novoAvaliador, name='novoAvaliador'),
-    path('avaliadores/',views.listAvaliadores, name='listAvaliadores'),
+    path('novoAvaliador/', views.novoAvaliador, name='novoAvaliador'),
+    path('avaliadores/', views.listAvaliadores, name='listAvaliadores'),
     path('deletarAvaliador/<int:id>', views.deleteAvaliador, name='deletarAvaliador'),
+    path('alterarAvaliador/<int:id>', views.alterarAvaliador, name='alterarAvaliador'),
 
-    path('novoPremio/',views.novoPremio, name='novoPremio'),
-    path('premios/',views.listPremios, name='listPremios'),
+    path('novoPremio/', views.novoPremio, name='novoPremio'),
+    path('premios/', views.listPremios, name='listPremios'),
     path('deletarPremio/<int:id>', views.deletePremio, name='deletarPremio'),
 
-    path('novoProjeto/',views.novoProjeto, name='novoProjeto'),
-    path('projetos/',views.listProjetos, name='listProjetos'),
+    path('novoProjeto/', views.novoProjeto, name='novoProjeto'),
+    path('projetos/', views.listProjetos, name='listProjetos'),
 
-    path('enviarProjeto/',views.enviarProjeto, name='enviarProjeto'),
-    path('projetosEnviados/',views.projetosEnviados, name='projetosEnviados'),
+    path('enviarProjeto/', views.enviarProjeto, name='enviarProjeto'),
+    path('projetosEnviados/', views.projetosEnviados, name='projetosEnviados'),
 
-    path('avaliarProjeto/',views.avaliarProjeto, name='avaliarProjeto'),
-    path('projetosAvaliados/',views.projetosAvaliados, name='projetosAvaliados')
+    path('avaliarProjeto/', views.avaliarProjeto, name='avaliarProjeto'),
+    path('projetosAvaliados/', views.projetosAvaliados, name='projetosAvaliados')
 ]
-
--- mysite/teste/views.py
+++ mysite/teste/views.py
@@ -5,40 +5,48 @@ from .serializer import *
 from rest_framework import viewsets
 import coreapi
 
+
 # Create your views here.
-#------- APIs
+# ------- APIs
 class AutorViewApi(viewsets.ModelViewSet):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
+
 
 class CronogramaViewApi(viewsets.ModelViewSet):
     queryset = Cronograma.objects.all()
     serializer_class = CronogramaSerializer
 
+
 class AvaliadorViewApi(viewsets.ModelViewSet):
     queryset = Avaliador.objects.all()
     serializer_class = AvaliadorSerializer
+
 
 class PremioViewApi(viewsets.ModelViewSet):
     queryset = Premio.objects.all()
     serializer_class = PremioSerializer
 
+
 class ProjetoViewApi(viewsets.ModelViewSet):
     queryset = Projeto.objects.all()
     serializer_class = ProjetoSerializer
 
+
 class ProjetoEnviadoViewApi(viewsets.ModelViewSet):
     queryset = EnviarProjeto.objects.all()
     serializer_class = EnviarProjetoSerializer
+
 
 class ProjetoAvaliadoViewApi(viewsets.ModelViewSet):
     queryset = AvaliarProjeto.objects.all()
     serializer_class = AvaliarProjetoSerializer
 
 
-#-----------------------------CRUDS-------------------------------------
+# -----------------------------CRUDS-------------------------------------
 def index(request):
     return HttpResponse("Hello Word")
+
 
 def novoAutor(request):
     form = AutorForm()
@@ -61,6 +69,42 @@ def novoAutor(request):
     elif request.method == 'GET':
         return render(request, 'Autor/novo_autor.html', {'form': form})
 
+
+def alterarAutor(request, id):
+    autor = Autor.objects.get(id=id)
+    form = AutorForm(instance=autor)
+
+    if request.method == 'GET':
+        return render(request, 'Autor/alterar_autor.html',
+                      {'form': form, 'autor': autor})
+    elif request.method == 'POST':
+        form = AutorForm(request.POST, instance=autor)
+        autor = form.save(commit=False)
+        autor.nome = form.cleaned_data['nome']
+        autor.idade = form.cleaned_data['idade']
+        autor.cpf = form.cleaned_data['cpf']
+        autor.telefone = form.cleaned_data['telefone']
+        autor.email = form.cleaned_data['email']
+        autor.save()
+
+        # Initialize a client & load the schema document
+        client = coreapi.Client()
+        schema = client.get("http://127.0.0.1:8000/teste/docs")
+
+        # Interact with the API endpoint
+        action = ["autores", "update"]
+        params = {
+            "id": id,
+            "nome": form.cleaned_data['nome'],
+            "idade": form.cleaned_data['idade'],
+            "cpf": form.cleaned_data['cpf'],
+            "telefone": form.cleaned_data['telefone'],
+            "email": form.cleaned_data['email'],
+        }
+        client.action(schema, action, params=params)
+        return redirect('listAutores')
+
+
 def listAutores(request):
     client = coreapi.Client()
     schema = client.get("http://127.0.0.1:8000/teste/docs")
@@ -68,6 +112,7 @@ def listAutores(request):
     action = ["autores", "list"]
     result = client.action(schema, action)
     return render(request, 'Autor/list_autores.html', {'autores': result})
+
 
 def deleteAutor(request, id):
     client = coreapi.Client()
@@ -80,6 +125,7 @@ def deleteAutor(request, id):
     result = client.action(schema, action, params=params)
     return redirect('listAutores')
 
+
 def listCronogramas(request):
     # Initialize a client & load the schema document
     client = coreapi.Client()
@@ -89,6 +135,7 @@ def listCronogramas(request):
     action = ["cronogramas", "list"]
     result = client.action(schema, action)
     return render(request, 'Cronograma/list_cronogramas.html', {'cronogramas': result})
+
 
 def novoCronograma(request):
     form = CronogramaForm()
@@ -109,6 +156,38 @@ def novoCronograma(request):
     elif request.method == 'GET':
         return render(request, 'Cronograma/novo_cronograma.html', {'form': form})
 
+
+def alterarCronograma(request, id):
+    cronograma = Cronograma.objects.get(id=id)
+    form = CronogramaForm(instance=cronograma)
+
+    if request.method == 'GET':
+        return render(request, 'Cronograma/alterar_cronograma.html',
+                      {'form': form, 'cronograma': cronograma})
+    elif request.method == 'POST':
+        form = CronogramaForm(request.POST, instance=cronograma)
+        cronograma = form.save(commit=False)
+        cronograma.descricao = form.cleaned_data['descricao']
+        cronograma.dataInicio = form.cleaned_data['dataInicio']
+        cronograma.dataFim = form.cleaned_data['dataFim']
+        cronograma.save()
+
+        # Initialize a client & load the schema document
+        client = coreapi.Client()
+        schema = client.get("http://127.0.0.1:8000/teste/docs")
+
+        # Interact with the API endpoint
+        action = ["cronogramas", "update"]
+        params = {
+            "id": id,
+            "dataInicio": form.cleaned_data['dataInicio'].isoformat(),
+            "dataFim": form.cleaned_data['dataFim'].isoformat(),
+            "descricao": form.cleaned_data['descricao'],
+        }
+        client.action(schema, action, params=params)
+        return redirect('listCronogramas')
+
+
 def deleteCronograma(request, id):
     client = coreapi.Client()
     schema = client.get("http://127.0.0.1:8000/teste/docs/")
@@ -121,6 +200,7 @@ def deleteCronograma(request, id):
     client.action(schema, action, params=params)
     return redirect('listCronogramas')
 
+
 def listAvaliadores(request):
     client = coreapi.Client()
     schema = client.get("http://127.0.0.1:8000/teste/docs")
@@ -128,6 +208,7 @@ def listAvaliadores(request):
     action = ["avaliadores", "list"]
     result = client.action(schema, action)
     return render(request, 'Avaliador/list_avaliadores.html', {'avaliadores': result})
+
 
 def novoAvaliador(request):
     form = AvaliadorForm()
@@ -149,6 +230,40 @@ def novoAvaliador(request):
     elif request.method == 'GET':
         return render(request, 'Avaliador/novo_avaliador.html', {'form': form})
 
+
+def alterarAvaliador(request, id):
+    avaliador = Avaliador.objects.get(id=id)
+    form = AvaliadorForm(instance=avaliador)
+
+    if request.method == 'GET':
+        return render(request, 'Avaliador/alterar_avaliador.html',
+                      {'form': form, 'avaliador': avaliador})
+    elif request.method == 'POST':
+        form = AvaliadorForm(request.POST, instance=avaliador)
+        avaliador = form.save(commit=False)
+        avaliador.nome = form.cleaned_data['nome']
+        avaliador.idade = form.cleaned_data['idade']
+        avaliador.cpf = form.cleaned_data['cpf']
+        avaliador.areaFormacao = form.cleaned_data['areaFormacao']
+        avaliador.save()
+
+        # Initialize a client & load the schema document
+        client = coreapi.Client()
+        schema = client.get("http://127.0.0.1:8000/teste/docs")
+
+        # Interact with the API endpoint
+        action = ["avaliadores", "update"]
+        params = {
+            "id": id,
+            "nome": form.cleaned_data['nome'],
+            "idade": form.cleaned_data['idade'],
+            "cpf": form.cleaned_data['cpf'],
+            "areaFormacao": form.cleaned_data['areaFormacao'],
+        }
+        client.action(schema, action, params=params)
+        return redirect('listAvaliadores')
+
+
 def deleteAvaliador(request, id):
     client = coreapi.Client()
     schema = client.get("http://127.0.0.1:8000/teste/docs/")
@@ -160,6 +275,7 @@ def deleteAvaliador(request, id):
     result = client.action(schema, action, params=params)
     return redirect('listAvaliadores')
 
+
 def listPremios(request):
     client = coreapi.Client()
     schema = client.get("http://127.0.0.1:8000/teste/docs")
@@ -167,6 +283,7 @@ def listPremios(request):
     action = ["premios", "list"]
     result = client.action(schema, action)
     return render(request, 'Premio/list_premios.html', {'premios': result})
+
 
 def novoPremio(request):
     form = PremioForm()
@@ -188,6 +305,40 @@ def novoPremio(request):
     elif request.method == 'GET':
         return render(request, 'Premio/novo_premio.html', {'form': form})
 
+
+def alterarPremio(request, id):
+    premio = Premio.objects.get(id=id)
+    form = PremioForm(instance=premio)
+
+    if request.method == 'GET':
+        return render(request, 'Premio/alterar_premio.html',
+                      {'form': form, 'premio': premio})
+    elif request.method == 'POST':
+        form = PremioForm(request.POST, instance=premio)
+        premio = form.save(commit=False)
+        premio.nome = form.cleaned_data['nome']
+        premio.idade = form.cleaned_data['idade']
+        premio.cpf = form.cleaned_data['cpf']
+        premio.areaFormacao = form.cleaned_data['areaFormacao']
+        premio.save()
+
+        # Initialize a client & load the schema document
+        client = coreapi.Client()
+        schema = client.get("http://127.0.0.1:8000/teste/docs")
+
+        # Interact with the API endpoint
+        action = ["autores", "update"]
+        params = {
+            "id": id,
+            "nome": form.cleaned_data['nome'],
+            "descricao": form.cleaned_data['descricao'],
+            "ano": form.cleaned_data['ano'],
+            "cronograma_fk": form.cleaned_data['cronograma_fk'].id,
+        }
+        client.action(schema, action, params=params)
+        return redirect('listAvaliadores')
+
+
 def deletePremio(request, id):
     client = coreapi.Client()
     schema = client.get("http://127.0.0.1:8000/teste/docs/")
@@ -198,6 +349,7 @@ def deletePremio(request, id):
     }
     result = client.action(schema, action, params=params)
     return redirect('listPremios')
+
 
 def listProjetos(request):
     client = coreapi.Client()
@@ -240,10 +392,12 @@ def novoProjeto(request):
     data['form'] = form
     return render(request, 'Projeto/novo_projeto.html', data)
 
+
 def projetosEnviados(request):
     data = {}
     data['projetosEnviados'] = EnviarProjeto.objects.all()
     return render(request, 'Projeto/projetos_enviados.html', data)
+
 
 def enviarProjeto(request):
     data = {}
@@ -256,10 +410,12 @@ def enviarProjeto(request):
     data['form'] = form
     return render(request, 'Projeto/enviar_projeto.html', data)
 
+
 def projetosAvaliados(request):
     data = {}
     data['projetosAvaliados'] = AvaliarProjeto.objects.all()
     return render(request, 'Projeto/projetos_avaliados.html', data)
+
 
 def avaliarProjeto(request):
     data = {}
@@ -271,4 +427,3 @@ def avaliarProjeto(request):
 
     data['form'] = form
     return render(request, 'Projeto/avaliar_projeto.html', data)
-
